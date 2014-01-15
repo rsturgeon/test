d4385e020f96875d1b6edcaa8555c0afe2ba2262
Roy <user@dhcp-10-32-1-144.hq.tidemark.net>
2014-01-15 13:51:16 -0800

    Modified UserService

--- basic-api/src/main/java/app/tomcat/api/UserService.java
+++ basic-api/src/main/java/app/tomcat/api/UserService.java
@@ -11,14 +11,19 @@ import javax.ws.rs.core.MediaType;
  * Created by nhristov on 1/14/14.
  */
 public interface UserService {
-    @Path("users")
-    @GET
-    @Produces({MediaType.APPLICATION_JSON})
-    String listUsers();
+  @Path("users")
+  @GET
+  @Produces({MediaType.APPLICATION_JSON})
+  String listUsers();
 
-    @Path("user/{id}")
-    @DELETE
-    @Produces({MediaType.APPLICATION_JSON})
-    String deleteUser(@PathParam("id") String id);
+  @Path("user/{id}")
+  @DELETE
+  @Produces({MediaType.APPLICATION_JSON})
+  String deleteUser(@PathParam("id") String id);
+
+  @Path("user/{id}")
+  @GET
+  @Produces({MediaType.APPLICATION_JSON})
+  String getUser(@PathParam("id") String id);
 
 }
